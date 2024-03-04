9483e2d779dc8bfff669aeb4aac525184a948f1c
Joe <149742344+Hunk-OS@users.noreply.github.com>
2024-03-04 04:16:30 -0500

    Second commit, Fixed Issue.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-try:
-    height = float(input("Enter your height in meters: "))
-    weight = float(input("Enter your weight in Kilograms: "))
+while True:
 
-    BMI = weight / round(height**2)
-    r_BMI = round(BMI, 3)
+    try:
+        height = float(input("Enter your height in meters: "))
+        weight = float(input("Enter your weight in Kilograms: "))
 
-    if BMI < 18.5:
-        print(f"Your BMI is {r_BMI} and you are UNDERWEIGHT")
-    elif BMI < 25:
-        print(f"Your BMI is {r_BMI} and you are NORMAL WEIGHT")
-    elif BMI < 30:
-        print(f"Your BMI is {r_BMI} and you are slightly OVERWEIGHT")
-    elif BMI < 35:
-        print(f"Your BMI is {r_BMI} and you are OBESE")
-    else:
-        print(f"Your BMI is {r_BMI}, You are clincally OBESE")
+        BMI = weight / round(height**2)
+        r_BMI = round(BMI, 3)
 
-except ValueError:
-    print("Please enter valid numeric values for height and weight.")
+        if BMI < 18.5:
+            print(f"Your BMI is {r_BMI} and you are UNDERWEIGHT")
+        elif BMI < 25:
+            print(f"Your BMI is {r_BMI} and you are NORMAL WEIGHT")
+        elif BMI < 30:
+            print(f"Your BMI is {r_BMI} and you are slightly OVERWEIGHT")
+        elif BMI < 35:
+            print(f"Your BMI is {r_BMI} and you are OBESE")
+        else:
+            print(f"Your BMI is {r_BMI}, You are clincally OBESE")
+
+        break # Exits the loop if the inputs are valid
+
+    except ValueError:
+        print("Please enter valid numeric values for height and weight.")
  
