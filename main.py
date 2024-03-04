8f6cdddc273f9645c5a6c2d22e5244513c4d0cca
Joe <149742344+Hunk-OS@users.noreply.github.com>
2024-03-04 04:09:29 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+try:
+    height = float(input("Enter your height in meters: "))
+    weight = float(input("Enter your weight in Kilograms: "))
+
+    BMI = weight / round(height**2)
+    r_BMI = round(BMI, 3)
+
+    if BMI < 18.5:
+        print(f"Your BMI is {r_BMI} and you are UNDERWEIGHT")
+    elif BMI < 25:
+        print(f"Your BMI is {r_BMI} and you are NORMAL WEIGHT")
+    elif BMI < 30:
+        print(f"Your BMI is {r_BMI} and you are slightly OVERWEIGHT")
+    elif BMI < 35:
+        print(f"Your BMI is {r_BMI} and you are OBESE")
+    else:
+        print(f"Your BMI is {r_BMI}, You are clincally OBESE")
+
+except ValueError:
+    print("Please enter valid numeric values for height and weight.")
+ 
